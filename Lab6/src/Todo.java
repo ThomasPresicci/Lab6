fe8ef203cdcbc5ed88ad63a588a294950103114f
Thomas Presicci <thomaspresicci@gmail.com>
2019-02-21 13:41:51 -0600

    Listed tasks and time estimates

---
+++ Lab6/src/Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-02-21
+OU ID_______________: 113457929
+OU EMAIL____________: thomaspresicci@ou.edu
+
+Tasks:
+1. Implement Rectangle.java and Square.java
+2. Finish implementation Shape.java and all child classes
+3. Implement ShapeAreaComparator.java and ShapePerimeterComparator.java
+4. Implement ShapeSorter.java
+5. Testing
+
+Estimated Time (minutes):
+1. 5
+2. 15
+3. 30
+4. 15
+5. 15
+
+Actual Time (minutes):
+1. 
+2. 
+3. 
+4. 
+5. 
+*/}
